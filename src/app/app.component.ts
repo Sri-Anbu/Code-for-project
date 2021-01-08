5176b76203eb592577815777a90561bc9cc43f02
sridharshini <Sri.Anbu@lntinfotech.com>
2021-01-08 18:11:33 +0530

    Front End Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute  } from '@angular/router'; 
+import { CookieService } from 'ngx-cookie-service'; 
+import { authService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ShopAppFrontEnd';
+  logintoggle:boolean;
+  login:boolean;
+  user:string;
+  constructor(private router: Router, public activatedRoute: ActivatedRoute,private cookieService:CookieService,private authservice:authService) {
+    this.user=this.cookieService.get('username');
+    console.log(this.user);
+    }
+ 
+   
+
+  logout(): void {
+    this.router.navigate(['']);
+    this.cookieService.deleteAll();
+    this.authservice.isLogged=false;
+    
+  }
 }
